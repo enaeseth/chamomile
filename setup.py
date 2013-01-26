e7caf49ff4ff57c2c2aa46cc354c4d49e74445dd
Eric Naeseth <eric@naeseth.com>
2013-01-26 01:11:16 -0800

    Advertise PyPy support in PyPI classifiers

--- setup.py
+++ setup.py
@@ -38,5 +38,7 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
     ]
 )
